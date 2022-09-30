a90680193331d9102b5f4eac71eb093e17250703
sunghee <hsh1352@donga.ac.kr>
2022-09-30 19:31:25 +0900

    commit2

---
+++ java_basic/src/ch08/MainTest2.java
@@ -0,0 +1,15 @@
+package ch08;
+
+public class MainTest2 {
+
+	public static void main(String[] args) {
+		// 학생 클래스는 메모리에 올려주세요
+		// new 라는 키워드를 사용하면 heap 메모리 영역에 올라감 
+		Student s1 = new Student(); 
+		s1.studnetName = "김철수";
+		s1.height = 180;
+		s1.weight = 80;
+
+	}
+
+}
---
+++ java_basic/src/ch08/Student.java
@@ -0,0 +1,10 @@
+package ch08;
+
+public class Student {
+	// 상태 ( 멤버 변수 )
+	String studnetName;
+	int height;
+	int weight;
+	
+
+}
